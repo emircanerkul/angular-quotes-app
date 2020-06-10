7bfceea4c314b8ca9ee8f4f28cce50457c1fd8fc
emircanerkul <develop@emircanerkul.com>
2020-06-10 16:50:50 +0300

    Profile background changer limitation added

--- src/app/component/profile-background/profile-background.component.ts
+++ src/app/component/profile-background/profile-background.component.ts
@@ -2,8 +2,9 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { ColorModule } from 'src/app/modules/color/color.module';
 import { AuthService } from 'src/app/service/auth/auth.service';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { take } from 'rxjs/operators';
+import { take, catchError } from 'rxjs/operators';
 import { ToastController } from '@ionic/angular';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-profile-background',
@@ -19,6 +20,7 @@ import { ToastController } from '@ionic/angular';
 })
 export class ProfileBackgroundComponent {
   @Input() color;
+  canRefreshBackgroundColor: boolean = true;
 
   constructor(
     public colorModule: ColorModule,
@@ -28,30 +30,39 @@ export class ProfileBackgroundComponent {
   ) {}
 
   refreshBackgroundColor() {
-    this.auth.user$.pipe(take(1)).subscribe((e) => {
-      let color;
-      do {
-        color = this.colorModule.getRandomColor();
-      } while (color == e.color || color == undefined);
-      console.log(color + ':' + e.uid);
+    this.canRefreshBackgroundColor = false;
 
-      this.afStore
-        .collection('users')
-        .doc(e.uid)
-        .update({ color })
-        .catch((e) => {
-          console.log(e);
-          return;
-        })
-        .finally(() => {
-          this.toastController
-            .create({
-              message: 'Background Changed!',
-              duration: 1000
-            })
-            .then((e) => e.present());
-        });
-    });
+    this.auth.user$.pipe(take(1)).subscribe(
+      (e) => {
+        let color;
+        do {
+          color = this.colorModule.getRandomColor();
+        } while (color == e.color || color == undefined);
+
+        this.afStore
+          .collection('users')
+          .doc(e.uid)
+          .update({ color })
+          .catch((e) => {
+            this.canRefreshBackgroundColor = true;
+            console.log(e);
+            return;
+          })
+          .finally(() => {
+            setTimeout(() => (this.canRefreshBackgroundColor = true), 10000);
+            this.toastController
+              .create({
+                message: 'Background Changed!',
+                duration: 1000
+              })
+              .then((e) => e.present());
+          });
+      },
+      catchError((err) => {
+        this.canRefreshBackgroundColor = true;
+        throw 'error in source. Details: ' + err;
+      })
+    );
     return;
   }
 }
